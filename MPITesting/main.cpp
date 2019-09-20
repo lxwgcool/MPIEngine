4ae28f1b93d260cdb9d955ad9035111bbc4ae2ce
Xin Li <lxwgcool@gmail.com>
2019-09-20 17:29:29 -0400

    Change typo

--- MPITesting/main.cpp
+++ MPITesting/main.cpp
@@ -21,7 +21,7 @@ int main(int argc,char *argv[])
              << "1: Boost any exe with multiple processes by using different argument for each process." << endl
              << "2: Usage: (1) MPIEngine -n <number of cores> your_program" << endl
              << "          (2) Fill the parameters into \"config.ini\". Each line is corresponding to all the arguments of one process." << endl
-             << "3: Example: there are three examples, and each of them applys 8 cores to run the program with multiple parameters of each." << endl
+             << "3: Example: there are three examples, and each of them applies 8 cores to run the program with multiple parameters of each." << endl
              << "     E.g.1: mpirun -n 8 MPIEngine config.ini" << endl
              << "     E.g.2: mpirun -n 8 MPIEngine myprogram.exe config.ini" << endl
              << "     E.g.3: mpirun -n 8 MPIEngine python mySourceCode.py config.ini" << endl;
